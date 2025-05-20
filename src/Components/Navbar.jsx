02a6662c16516815f264b5a12218f1feaa7e465d
Brenda Tapari <99502@sistemas.frc.utn.edu.ar>
2025-05-20 18:15:41 -0300

    agregue un avatar porque hay un usuario logueado

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,27 @@
 export default function Navbar() {
   return (
     <>
-      <div className="navbar bg-primary shadow-sm">
-        <a
-          className="btn btn-ghost text-3xl text-white hover:bg-primary/80 "
-          onClick={() =>
-            alert("Hola! Gracias por darte cuenta que es un botón, un saludo! ")
-          }
-        >
-          Red Sísmica
-        </a>
+      <div className="navbar bg-primary text-primary-content shadow-lg px-4 py-2">
+        <div className="flex-1">
+          <a
+            className="btn btn-ghost text-3xl नॉर्मल-case hover:bg-primary-focus"
+            onClick={() =>
+              alert("Hola! Gracias por darte cuenta que es un botón, un saludo! ")
+            }
+          >
+            Red Sísmica
+          </a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="avatar">
+            <div className="w-14 h-14 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2">
+              <img
+                src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
+                alt="User Avatar"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
